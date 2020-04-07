055d9d4340f0d32110879d51d54bde9f1d5d6064
paidaxing444 <12qwaszx>
2020-04-07 16:08:59 +0800

    20200407-zlp-2

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dashboard-editor-container">
 
-    <el-row :gutter="32" >
+    <el-row :gutter="32">
       <el-col :xs="24" :sm="24" :lg="8">
         <div class="chart-wrapper">
           <raddar-chart />
