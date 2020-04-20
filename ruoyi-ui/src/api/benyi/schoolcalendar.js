d847efd9bd92e862350ca827edac54d37e8567bb
paidaxing444 <12qwaszx>
2020-04-20 17:28:41 +0800

    20200420-zlp-2 园历创建

---
+++ ruoyi-ui/src/api/benyi/schoolcalendar.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询园历管理列表
+export function listSchoolcalendar(query) {
+  return request({
+    url: '/benyi/schoolcalendar/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询园历管理详细
+export function getSchoolcalendar(id) {
+  return request({
+    url: '/benyi/schoolcalendar/' + id,
+    method: 'get'
+  })
+}
+
+// 新增园历管理
+export function addSchoolcalendar(data) {
+  return request({
+    url: '/benyi/schoolcalendar',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改园历管理
+export function updateSchoolcalendar(data) {
+  return request({
+    url: '/benyi/schoolcalendar',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除园历管理
+export function delSchoolcalendar(id) {
+  return request({
+    url: '/benyi/schoolcalendar/' + id,
+    method: 'delete'
+  })
+}
+
+// 导出园历管理
+export function exportSchoolcalendar(query) {
+  return request({
+    url: '/benyi/schoolcalendar/export',
+    method: 'get',
+    params: query
+  })
+}
