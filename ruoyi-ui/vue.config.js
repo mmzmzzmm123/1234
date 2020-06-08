a31427127c634324cbb289e84cce0f23ce05e976
Live <1005297262@qq.com>
2020-06-08 18:23:07 +0800

    后台管理系统禁止所有搜索引擎收录配置

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -118,6 +118,10 @@ module.exports = {
               }
             })
           config.optimization.runtimeChunk('single')
+        },
+        {
+             from: path.resolve(__dirname, './public/robots.txt'),//防爬虫文件
+             to:'./',//到根目录下
         }
       )
   }
