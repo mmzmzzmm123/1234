60887ca22fa245d3e17dce726c84eec14a70f3d0
paidaxing444 <12qwaszx>
2020-08-11 17:10:11 +0800

    幼儿入园系列文件-列表页

---
+++ ruoyi-ui/src/views/benyi/child_files/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-container">
+    <el-row :gutter="12">
+      <el-col :span="4">
+        <el-card shadow="hover">家长声明</el-card>
+      </el-col>
+      <el-col :span="4">
+        <el-card shadow="hover">日常生活记录表</el-card>
+      </el-col>
+      <el-col :span="4">
+        <el-card shadow="hover">幼儿编班通知</el-card>
+      </el-col>
+      <el-col :span="4">
+        <el-card shadow="hover">幼儿参加园外活动许可</el-card>
+      </el-col>
+      <el-col :span="4">
+        <el-card shadow="hover">幼儿参与拍照与拍摄许可</el-card>
+      </el-col>
+      <el-col :span="4">
+        <el-card shadow="hover">幼儿健康说明表</el-card>
+      </el-col>
+    </el-row>
+    <el-row :gutter="12">
+      <el-col :span="4">
+        <el-card shadow="hover">幼儿就医许可</el-card>
+      </el-col>
+      <el-col :span="4">
+        <el-card shadow="hover">幼儿生活用品领取单</el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
