a2dfd2c6ae969fe185fe79a3ff4b3e1c39450575
sk1551 <15175617877@163.com>
2020-09-04 13:47:43 +0800

    优化考核审核

--- ruoyi-ui/src/views/jxzxkhgl/jxzxxxsh/index.vue
+++ ruoyi-ui/src/views/jxzxkhgl/jxzxxxsh/index.vue
@@ -75,7 +75,14 @@
       <el-table-column type="selection" width="55" align="center" :selectable="isShow" />
       <!-- <el-table-column label="聘任校" align="center" prop="tsbzJxjsjbxx.prdwmc" /> -->
       <el-table-column label="考核方案" align="center" prop="faid" :formatter="faFormat" />
-      <el-table-column label="教师" align="center" prop="tsbzJxjsjbxx.name" />
+      <el-table-column label="教师" align="center" prop="tsbzJxjsjbxx.name">
+        <template slot-scope="scope">
+          <el-button
+          type="text"
+          @click="handleDetail(scope.row)">
+          {{ scope.row.tsbzJxjsjbxx.name }}</el-button>  
+        </template>  
+      </el-table-column>
       <el-table-column label="状态" align="center" prop="status" :formatter="statusFormat" />
       <el-table-column label="校级审核意见" align="center" prop="xjshyj" :formatter="xjshyjFormat" />
       <el-table-column label="校级审核建议" align="center" prop="xjshjy" />
@@ -121,16 +128,6 @@
           <el-input v-model="jsxm" :disabled="true" />
           <el-input v-model="form.jsid" v-if="false" />
         </el-form-item>
-        <!-- <el-form-item label="状态" prop="status">
-          <el-select v-model="form.status" placeholder="请选择状态">
-            <el-option
-              v-for="dict in statusOptions"
-              :key="dict.dictValue"
-              :label="dict.dictLabel"
-              :value="dict.dictValue"
-            ></el-option>
-          </el-select>
-        </el-form-item>-->
         <el-form-item label="校级审核意见" prop="xjshyj">
           <el-select v-model="form.xjshyj" placeholder="请选择校级审核意见">
             <el-option
@@ -143,6 +140,37 @@
         </el-form-item>
         <el-form-item label="校级审核建议" prop="xjshjy">
           <el-input v-model="form.xjshjy" type="textarea" placeholder="请输入内容" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+
+    <!-- 教师详情页 -->
+    <el-dialog :title="title" :visible.sync="open1" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="120px">
+        <el-form-item label="教师" prop="jsid">
+          <el-input v-model="jsxm" :disabled="true" />
+        </el-form-item>
+        <el-form-item label="考核方案" prop="faid">
+          <el-select v-model="form.faid" placeholder="请选择方案" :disabled="true">
+            <el-option v-for="dict in faOptions" :key="dict.id" :label="dict.name" :value="dict.id"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="聘任校" prop="jsprx">
+          <el-input v-model="jsprx" :disabled="true" />
+        </el-form-item>
+        <el-form-item label="状态" prop="status">
+          <el-select v-model="form.status" placeholder="请选择状态" :disabled="true">
+            <el-option
+              v-for="dict in statusOptions"
+              :key="dict.dictValue"
+              :label="dict.dictLabel"
+              :value="dict.dictValue"
+            ></el-option>
+          </el-select>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -169,6 +197,8 @@ export default {
     return {
       //教师姓名
       jsxm: "",
+      // 教师聘任校
+      jsprx: "",
       //默认方案id
       defaultFaid: "",
       // 遮罩层
@@ -189,6 +219,8 @@ export default {
       title: "",
       // 是否显示弹出层
       open: false,
+      // 教师详情弹出层
+      open1: false,
       // 状态字典
       statusOptions: [],
       // 校级审核意见字典
@@ -197,6 +229,7 @@ export default {
       qjshyjOptions: [],
       //方案
       faOptions: [],
+      
       // 查询参数
       queryParams: {
         pageNum: 1,
@@ -224,9 +257,6 @@ export default {
       form: {},
       // 表单校验
       rules: {
-        faid: [
-          { required: true, message: "方案编号不能为空", trigger: "blur" },
-        ],
         xjshyj: [
           { required: true, message: "方案编号不能为空", trigger: "blur" },
         ],
@@ -304,6 +334,7 @@ export default {
     // 取消按钮
     cancel() {
       this.open = false;
+      this.open1 = false;
       this.reset();
     },
     // 表单重置
@@ -353,6 +384,21 @@ export default {
         this.form = response.data;
         this.open = true;
         this.title = "修改考核审核过程";
+      });
+    },
+    /** 详情按钮操作 */
+    handleDetail(row) {
+      this.reset();
+      const id = row.id || this.ids;
+      if (id == null || id == "") {
+        return this.msgError("当前教师未提交数据，无法查看详情！");
+      }
+      getJzxzkhsh(id).then((response) => {
+        this.jsxm = response.data.tsbzJxjsjbxx.name;
+        this.jsprx = response.data.tsbzJxjsjbxx.prdwmc;
+        this.form = response.data;
+        this.open1 = true;
+        this.title = "教师详情";
       });
     },
     /** 提交按钮 */
