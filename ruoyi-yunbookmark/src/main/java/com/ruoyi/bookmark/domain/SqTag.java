046bd8c032544894bffbba7b7db76fe0acdd76d0
WangHao <43278047@qq.com>
2020-09-05 13:29:22 +0800

    书签TAG表新增字段

--- ruoyi-yunbookmark/src/main/java/com/ruoyi/bookmark/domain/SqTag.java
+++ ruoyi-yunbookmark/src/main/java/com/ruoyi/bookmark/domain/SqTag.java
@@ -1,17 +1,20 @@
 package com.ruoyi.bookmark.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.common.annotation.Excel;
 import com.ruoyi.common.core.domain.BaseEntity;
 
+import java.util.Date;
+
 /**
  * 书签_标签对象 sq_tag
- * 
+ *
  * @author ruoyi
- * @date 2020-09-04
+ * @date 2020-09-05
  */
-public class SqTag extends BaseEntity
+public class SqTag
 {
     private static final long serialVersionUID = 1L;
 
@@ -26,32 +29,96 @@ public class SqTag extends BaseEntity
     @Excel(name = "引用数量")
     private Integer icount;
 
-    public void setId(Long id) 
+    /** 谁增加的该标签 */
+    @Excel(name = "谁增加的该标签")
+    private Long userId;
+
+    /** S系统标签,P书签标签,B便签书签 */
+    @Excel(name = "S系统标签,P书签标签,B便签书签")
+    private String tagType;
+
+    /** 链接 */
+    @Excel(name = "链接")
+    private String url;
+
+    /** 状态 */
+    @Excel(name = "状态")
+    private Integer status;
+
+    /** 创建时间 */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    public void setId(Long id)
     {
         this.id = id;
     }
 
-    public Long getId() 
+    public Long getId()
     {
         return id;
     }
-    public void setName(String name) 
+    public void setName(String name)
     {
         this.name = name;
     }
 
-    public String getName() 
+    public String getName()
     {
         return name;
     }
-    public void setIcount(Integer icount) 
+    public void setIcount(Integer icount)
     {
         this.icount = icount;
     }
 
-    public Integer getIcount() 
+    public Integer getIcount()
     {
         return icount;
+    }
+    public void setUserId(Long userId)
+    {
+        this.userId = userId;
+    }
+
+    public Long getUserId()
+    {
+        return userId;
+    }
+    public void setTagType(String tagType)
+    {
+        this.tagType = tagType;
+    }
+
+    public String getTagType()
+    {
+        return tagType;
+    }
+    public void setUrl(String url)
+    {
+        this.url = url;
+    }
+
+    public String getUrl()
+    {
+        return url;
+    }
+    public void setStatus(Integer status)
+    {
+        this.status = status;
+    }
+
+    public Integer getStatus()
+    {
+        return status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
     }
 
     @Override
@@ -60,6 +127,10 @@ public class SqTag extends BaseEntity
             .append("id", getId())
             .append("name", getName())
             .append("icount", getIcount())
+            .append("userId", getUserId())
+            .append("tagType", getTagType())
+            .append("url", getUrl())
+            .append("status", getStatus())
             .append("createTime", getCreateTime())
             .toString();
     }
