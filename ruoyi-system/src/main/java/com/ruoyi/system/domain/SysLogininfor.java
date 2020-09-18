c6442d5b2d557334a7b940793865cd38fdc543fe
WangHao <43278047@qq.com>
2020-09-19 00:04:24 +0800

    lombok注解文档

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysLogininfor.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysLogininfor.java
@@ -8,9 +8,10 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 系统访问记录表 sys_logininfor
- * 
+ *
  * @author ruoyi
  */
+
 public class SysLogininfor extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
