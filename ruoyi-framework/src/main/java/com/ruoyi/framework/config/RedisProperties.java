9c89e0066f01e4fbfdadaca51975efedeeaf2bd7
码上风骚 <xuc5566@gmail.com>
2020-10-17 11:56:53 +0800

    增加分布式锁

---
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/RedisProperties.java
@@ -0,0 +1,16 @@
+package com.ruoyi.framework.config;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisProperties {
+    private String host;
+    private int port;
+    private String password;
+    private int database;
+}
