be06481f7f8671632f47a733a23ebaf34b1eb098
yelifeng <xuc5566@qq.com>
2020-10-20 18:13:05 +0800

    增加参数传递的公用类

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/ServerController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/ServerController.java
@@ -1,12 +1,14 @@
 package com.ruoyi.web.controller.monitor;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.framework.web.domain.Server;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * 服务器监控
@@ -24,5 +26,19 @@ public class ServerController extends BaseController
         Server server = new Server();
         server.copyTo();
         return AjaxResult.success(server);
+    }
+
+
+    /**
+     * AbstractController 使用示例，传递参数可以用parseRequestParamsForm来代替
+     * @param params
+     * @param request
+     * @param headers
+     * @return
+     */
+    @PostMapping("/login")
+    public AjaxResult login(@RequestBody(required = true) Map<String, Object> params, HttpServletRequest request, @RequestHeader HttpHeaders headers) {
+        Server userLoginForm = parseRequestParamsForm(params, Server.class);
+        return AjaxResult.success(userLoginForm);
     }
 }
