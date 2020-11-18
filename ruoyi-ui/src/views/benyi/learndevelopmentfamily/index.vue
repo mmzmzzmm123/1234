d5dd1f11a96addb7c78cf3a32e7e39e70ec1ffde
zhanglipeng <chinazhanglipeng@qq.com>
2020-11-18 16:22:39 +0800

    优化

--- ruoyi-ui/src/views/benyi/learndevelopmentfamily/index.vue
+++ ruoyi-ui/src/views/benyi/learndevelopmentfamily/index.vue
@@ -100,7 +100,7 @@
       <el-table-column
         label="操作"
         align="center"
-        width="100"
+        width="120"
         class-name="small-padding fixed-width"
       >
         <template slot-scope="scope">
