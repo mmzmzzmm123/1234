df6ca03ecedb59b627aa1dc5b38c2501ba85718d
huangdeliang <huangdeliang@skieer.com>
2020-11-24 22:00:46 +0800

    修改部署脚本

--- bin/build-server.sh
+++ bin/build-server.sh
@@ -4,4 +4,5 @@ git pull origin master
 mvn clean package -Dmaven.test.skip=true
 rm -f /home/api/manage.shengtangdiet.com/st-manage.jar
 cp -f ./ruoyi-admin/target/ruoyi-admin.jar /home/api/manage.shengtangdiet.com/st-manage.jar
-systemctl restart st-manage.service
+systemctl stop st-manage.service
+systemctl start st-manage.service
