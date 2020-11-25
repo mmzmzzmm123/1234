5bd1af0889108220b46135ff9c6be03464165ffd
huangdeliang <huangdeliang@skieer.com>
2020-11-25 22:01:49 +0800

    修改脚本

--- bin/build-test-server.sh
+++ bin/build-test-server.sh
@@ -4,4 +4,4 @@ git pull
 mvn clean package -Dmaven.test.skip=true -P dev
 rm -f /home/api/manage.shengtangdiet.com/test/stdiet-admin.jar
 cp -f ./stdiet-admin/target/stdiet-admin.jar /home/api/manage.shengtangdiet.com/test/
-./ry.sh restart /home/api/manage.shengtangdiet.com/test/
+nohup java -jar /home/api/manage.shengtangdiet.com/test/stdiet-admin.jar
