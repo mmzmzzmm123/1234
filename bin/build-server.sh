8d1519a3e8a1c60243d2c1db06c7e95c440b3049
huangdeliang <huangdeliang@skieer.com>
2020-11-25 21:53:28 +0800

    修改脚本

--- bin/build-server.sh
+++ bin/build-server.sh
@@ -4,4 +4,4 @@ git pull origin master
 mvn clean package -Dmaven.test.skip=true -P prod
 rm -f /home/api/manage.shengtangdiet.com/st-manage.jar
 cp -f ./stdiet-admin/target/stdiet-admin.jar /home/api/manage.shengtangdiet.com/st-manage.jar
-nohup java -jar /home/api/manage.shengtangdiet.com/st-manage.jar
+systemctl reload st-manage.service
--- bin/build-test-server.sh
+++ bin/build-test-server.sh
@@ -2,6 +2,6 @@
 cd /home/workspace/ShengTangManage
 git pull
 mvn clean package -Dmaven.test.skip=true -P dev
-rm -f /home/api/manage.shengtangdiet.com/test/st-manage.jar
-cp -f ./stdiet-admin/target/stdiet-admin.jar /home/api/manage.shengtangdiet.com/test/st-manage.jar
-nohup java -jar /home/api/manage.shengtangdiet.com/test/st-manage.jar
+rm -f /home/api/manage.shengtangdiet.com/test/stdiet-admin.jar
+cp -f ./stdiet-admin/target/stdiet-admin.jar /home/api/manage.shengtangdiet.com/test/
+./ry.sh /home/api/manage.shengtangdiet.com/test/
