62e71a3843634f614d2b2ce6310f9918aaccbda6
huangdeliang <huangdeliang@skieer.com>
2020-11-25 21:40:53 +0800

    修改脚本

--- bin/build-test-server.sh
+++ bin/build-test-server.sh
@@ -3,5 +3,5 @@ cd /home/workspace/ShengTangManage
 git pull origin master
 mvn clean package -Dmaven.test.skip=true -P dev
 rm -f /home/api/manage.shengtangdiet.com/test/st-manage.jar
-cp -f ./ruoyi-admin/target/stdiet-admin.jar /home/api/manage.shengtangdiet.com/test/st-manage.jar
+cp -f ./stdiet-admin/target/stdiet-admin.jar /home/api/manage.shengtangdiet.com/test/st-manage.jar
 nohup java -jar /home/api/manage.shengtangdiet.co/test/st-manage.jar
