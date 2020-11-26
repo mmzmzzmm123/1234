8567c92bc985e682d83d057e540c68ec6169ba80
huangdeliang <huangdeliang@skieer.com>
2020-11-26 11:32:09 +0800

    更新脚本

--- bin/build-test-server.sh
+++ bin/build-test-server.sh
@@ -5,5 +5,5 @@ mvn clean package -Dmaven.test.skip=true -P dev
 rm -f /home/api/manage.shengtangdiet.com/test/stdiet-admin.jar
 cp -f ./stdiet-admin/target/stdiet-admin.jar /home/api/manage.shengtangdiet.com/test/
 
-netstat -nlp | grep :8091 | awk '{print $7}' | awk -F"/" '{ print $1 }' | kill
+kill -9 $(lsof -i tcp:8091 -t)
 nohup java -jar /home/api/manage.shengtangdiet.com/test/stdiet-admin.jar
