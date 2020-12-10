1efc043640051e80c7963c47304f974fce107b54
〝走走停停 <1103914483@qq.com>
2020-12-10 11:14:40 +0800

    添加xy工具js

--- ruoyi-ui/src/main.js
+++ ruoyi-ui/src/main.js
@@ -7,6 +7,8 @@ import 'normalize.css/normalize.css' // a modern alternative to CSS resets
 import Element from 'element-ui'
 import './assets/styles/element-variables.scss'
 
+import '@/assets/styles/Xy.scss' // XiaoYi css
+
 import '@/assets/styles/index.scss' // global css
 import '@/assets/styles/ruoyi.scss' // ruoyi css
 import App from './App'
