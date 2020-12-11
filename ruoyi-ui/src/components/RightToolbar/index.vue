d937d97f26e03895b2856586add8b42c69f18536
〝走走停停 <1103914483@qq.com>
2020-12-11 13:40:30 +0800

    update ruoyi-ui/src/components/RightToolbar/index.vue. 对搜索、刷新加入显示控制

--- ruoyi-ui/src/components/RightToolbar/index.vue
+++ ruoyi-ui/src/components/RightToolbar/index.vue
@@ -1,11 +1,11 @@
-<!-- @author Shiyn/   huangmx 20200807优化-->
+<!-- @author XY  20201211 优化 -->
 <template>
   <div class="top-right-btn">
     <el-row>
-      <el-tooltip class="item" effect="dark" :content="showSearch ? '隐藏搜索' : '显示搜索'" placement="top">
+      <el-tooltip class="item" effect="dark" :content="showSearch ? '隐藏搜索' : '显示搜索'" placement="top" v-if="search_btn">
         <el-button size="mini" circle icon="el-icon-search" @click="toggleSearch()" />
       </el-tooltip>
-      <el-tooltip class="item" effect="dark" content="刷新" placement="top">
+      <el-tooltip class="item" effect="dark" content="刷新" placement="top"  v-if="refresh_btn">
         <el-button size="mini" circle icon="el-icon-refresh" @click="refresh()" />
       </el-tooltip>
     </el-row>
@@ -19,6 +19,14 @@ export default {
   },
   props: {
     showSearch: {
+      type: Boolean,
+      default: true,
+    },
+    search_btn: {
+      type: Boolean,
+      default: true,
+    },
+    refresh_btn: {
       type: Boolean,
       default: true,
     },
