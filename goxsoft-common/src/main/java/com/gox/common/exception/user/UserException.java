f1d75dbd27988e77b29495fd218a03ca61b33070
zgqahah <894542543@qq.com>
2020-12-24 13:03:56 +0800

    modify package name

---
+++ goxsoft-common/src/main/java/com/gox/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package com.gox.common.exception.user;
+
+import com.gox.common.exception.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
