90030b76f63fea1dda71ed3ea933c3b49b9d20d0
huangdeliang <huangdeliang@skieer.com>
2021-02-01 09:20:28 +0800

    修改脚本

--- bin/build-server.sh
+++ bin/build-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 cd /home/workspace/ShengTangManage
-git pull origin master
+git pull
 mvn clean package -Dmaven.test.skip=true -P prod
 rm -f /home/api/manage.shengtangdiet.com/st-manage.jar
 cp -f ./stdiet-admin/target/stdiet-admin.jar /home/api/manage.shengtangdiet.com/st-manage.jar
--- bin/build-web.sh
+++ bin/build-web.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 cd /home/workspace/ShengTangManage/stdiet-ui
-git pull origin master
+git pull
 yarn
 yarn build:prod
 rm -rf /home/web/manage.shengtangdiet.com/dist
