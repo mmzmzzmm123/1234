eceadabfaa36b66f16803afb61e7050956c8cd08
huangdeliang <huangdeliang@skieer.com>
2021-02-01 15:23:00 +0800

    图片上传

--- stdiet-custom/src/main/java/com/stdiet/custom/utils/WxTokenUtils.java
+++ stdiet-custom/src/main/java/com/stdiet/custom/utils/WxTokenUtils.java
@@ -9,8 +9,9 @@ import com.stdiet.custom.domain.wechat.WxFileUploadResult;
 import com.thoughtworks.xstream.XStream;
 import org.apache.commons.io.IOUtils;
 
-import java.io.File;
-import java.io.InputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -29,7 +30,7 @@ public class WxTokenUtils {
 //    private static String appId = "wxaf10fe560ea043a0";
 //    private static String appSecret = "afb47e477337df23b7562c3c1f965826";
     private static String tokenUrl = "https://api.weixin.qq.com/cgi-bin/token";
-    private static String uploadMaterialUrl = "https://api.weixin.qq.com/cgi-bin/material/add_material";
+    private static String uploadMaterialUrl = "https://api.weixin.qq.com/cgi-bin/material/add_material?access_token=ACCESS_TOKEN&type=image";
 
 
     public static WxAccessToken fetchAccessToken() {
@@ -47,16 +48,92 @@ public class WxTokenUtils {
         }
     }
 
+    /**
+     * 模拟form表单的形式 ，上传文件 以输出流的形式把文件写入到url中，然后用输入流来获取url的响应
+     *
+     * @return String url的响应信息返回值
+     * @throws IOException
+     */
     public static WxFileUploadResult uploadImage(File file, String fileName, String accessToken) throws Exception {
-        String url = uploadMaterialUrl + "?access_token=" + accessToken + "&type=image";
-        HttpPostUtil post = new HttpPostUtil(url);
-        post.addParameter("media", file);
-        String resultStr = post.send();
-        JSONObject obj = JSONObject.parseObject(resultStr);
-        WxFileUploadResult result = JSONObject.toJavaObject(obj, WxFileUploadResult.class);
-        result.setMediaId(resultStr);
-        result.setUrl(url);
-        return result;
+        String url = uploadMaterialUrl.replaceAll("ACCESS_TOKEN", accessToken);;//微信上传图片的url
+        String result = null;
+        if (!file.exists() || !file.isFile()) {
+            throw new IOException("文件不存在");
+        }
+        // 第一部分
+        URL urlObj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+        // 设置关键值
+        con.setRequestMethod("POST"); // 以Post方式提交表单，默认get方式
+        con.setDoInput(true);
+        con.setDoOutput(true);
+        con.setUseCaches(false); // post方式不能使用缓存
+
+        // 设置请求头信息
+        con.setRequestProperty("Connection", "Keep-Alive");
+        con.setRequestProperty("Charset", "UTF-8");
+
+        // 设置边界
+        String BOUNDARY = "----------" + System.currentTimeMillis();
+        con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+
+        // 请求正文信息
+        // 第一部分：
+        StringBuilder sb = new StringBuilder();
+        sb.append("--"); // 必须多两道线
+        sb.append(BOUNDARY);
+        sb.append("\r\n");
+        sb.append("Content-Disposition: form-data;name=\"file\";filename=\"" + fileName + "\"\r\n");
+        sb.append("Content-Type:application/octet-stream\r\n\r\n");
+
+        byte[] head = sb.toString().getBytes("utf-8");
+        // 获得输出流
+        OutputStream out = new DataOutputStream(con.getOutputStream());
+        // 输出表头
+        out.write(head);
+
+        // 文件正文部分
+        // 把文件已流文件的方式 推入到url中
+        DataInputStream in = new DataInputStream(new FileInputStream(file));
+        int bytes = 0;
+        byte[] bufferOut = new byte[1024];
+        while ((bytes = in.read(bufferOut)) != -1) {
+            out.write(bufferOut, 0, bytes);
+        }
+        in.close();
+
+        // 结尾部分
+        byte[] foot = ("\r\n--" + BOUNDARY + "--\r\n").getBytes("utf-8");// 定义最后数据分隔线
+        out.write(foot);
+        out.flush();
+        out.close();
+
+        StringBuffer buffer = new StringBuffer();
+        BufferedReader reader = null;
+        try {
+            // 定义BufferedReader输入流来读取URL的响应
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                // System.out.println(line);
+                buffer.append(line);
+            }
+            if (result == null) {
+                result = buffer.toString();
+            }
+        } catch (IOException e) {
+            System.out.println("发送POST请求出现异常！" + e);
+            e.printStackTrace();
+            throw new IOException("数据读取异常");
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        JSONObject jsonObject = JSONObject.parseObject(result);
+
+        return JSONObject.toJavaObject(jsonObject, WxFileUploadResult.class);
     }
 
     /**
