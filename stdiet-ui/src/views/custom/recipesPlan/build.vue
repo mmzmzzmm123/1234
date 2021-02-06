c800fb1aa3f8b492bb3d3901ccf44bd43b46c5bc
huangdeliang <huangdeliang@skieer.com>
2021-02-06 19:29:33 +0800

    食谱制作界面

---
+++ stdiet-ui/src/views/custom/recipesPlan/build.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app-container">
+    <div class="content">
+      <div class="left"></div>
+      <div class="right"></div>
+    </div>
+  </div>
+</template>
+<script>
+import { getRecipesPlan } from "@/api/custom/recipesPlan";
+import { getOrder } from "@/api/custom/order";
+import { getCustomerPhysicalSignsByCusId } from "@/api/custom/customer";
+export default {
+  name: "BuildRecipies",
+  data() {
+    return {
+      healthyData: {},
+    };
+  },
+  created() {
+    getOrder(this.cusId).then((res) => {
+      console.log(res);
+      getCustomerPhysicalSignsByCusId(res.data.cusId).then((iRes) => {
+        this.healthyData = iRes.data;
+      });
+    });
+  },
+  components: {},
+  props: ["id", "cusId"],
+  methods: {},
+};
+</script>
+<style>
+.content .left {
+}
+.content .right {
+}
+</style>
