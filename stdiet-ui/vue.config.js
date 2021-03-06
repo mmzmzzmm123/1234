d7e5ae3732ce1d2807e0b3a98a1747ae231a43d1
huangdeliang <huangdeliang@skieer.com>
2021-03-06 11:34:15 +0800

    订单管理页面适配手机端

--- stdiet-ui/vue.config.js
+++ stdiet-ui/vue.config.js
@@ -34,7 +34,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8090`,
+        target: `http://localhost:8091`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
