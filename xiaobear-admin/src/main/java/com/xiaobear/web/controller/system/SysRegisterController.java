181f36a0d452cf9dc6b7f76b1c7c2ae406a1d35d
yhx19991001@gmail.com <yhx19991001@gmail.com>
2021-09-18 11:00:22 +0800

    创建分支初始化

---
+++ xiaobear-admin/src/main/java/com/xiaobear/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.xiaobear.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.xiaobear.common.core.controller.BaseController;
+import com.xiaobear.common.core.domain.AjaxResult;
+import com.xiaobear.common.core.domain.model.RegisterBody;
+import com.xiaobear.framework.web.service.SysRegisterService;
+import com.xiaobear.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
