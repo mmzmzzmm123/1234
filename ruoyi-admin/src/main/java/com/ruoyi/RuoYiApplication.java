b45dd7bf096118b170b1eb3da3a7cc69f32ea01d
libiao <libiao.866@163.com>
2021-10-24 16:32:13 +0800

    fix:mod:打印应用启动的服务器信息优化，直接打印，不再设置默认环境

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,9 +1,5 @@
 package com.ruoyi;
 
-import com.ruoyi.common.utils.DefaultProfileUtil;
-import com.ruoyi.common.utils.SpringContextUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +8,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 
-import java.lang.invoke.MethodHandles;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -28,31 +23,20 @@ public class RuoYiApplication
     private static final Logger syslog = LoggerFactory.getLogger(RuoYiApplication.class);
     public static void main(String[] args)
     {
-        // System.setProperty("spring.devtools.restart.enabled", "false");
-        //SpringApplication.run(RuoYiApplication.class, args);
-        //System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-        //        " .-------.       ____     __        \n" +
-        //        " |  _ _   \\      \\   \\   /  /    \n" +
-        //        " | ( ' )  |       \\  _. /  '       \n" +
-        //        " |(_ o _) /        _( )_ .'         \n" +
-        //        " | (_,_).' __  ___(_ o _)'          \n" +
-        //        " |  |\\ \\  |  ||   |(_,_)'         \n" +
-        //        " |  | \\ `'   /|   `-'  /           \n" +
-        //        " |  |  \\    /  \\      /           \n" +
-        //        " ''-'   `'-'    `-..-'              ");
-        appEnvInfo(args);
-    }
-    /**
-     * @Description: 打印应用启动环境信息
-     * @Author: libiao
-     * @Date: 2021/10/17 19:40
-     * @param args:
-     * @return: void
-     **/
-    public static void appEnvInfo(String[] args){
-        SpringApplication app = new SpringApplication(RuoYiApplication.class);
-        DefaultProfileUtil.addDefaultProfile(app);
-        ApplicationContext appc =  app.run(args);
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        ApplicationContext appc = SpringApplication.run(RuoYiApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+
+        // 打印应用启动环境信息
         Environment env = appc.getEnvironment();
         String ip = "localhost";
         try {
@@ -67,6 +51,5 @@ public class RuoYiApplication
                 env.getProperty("spring.profiles.active"),ip,
                 env.getProperty("server.port"));
         syslog.info("程序启动完成!");
-        SpringContextUtils.setStaticApplicationContext(appc);
     }
 }
