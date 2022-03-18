131719ef04914a7cdcc5a76a02ca00680dd56ab1
fury_128 <498055813@qq.com>
2022-03-18 10:58:42 +0800

    用户登陆超时，重新登陆后原路径参数丢失解决，问题地址： https://gitee.com/y_project/RuoYi-Vue/issues/I4XHZR

--- ruoyi-ui/src/permission.js
+++ ruoyi-ui/src/permission.js
@@ -45,7 +45,7 @@ router.beforeEach((to, from, next) => {
       // 在免登录白名单，直接进入
       next()
     } else {
-      next(`/login?redirect=${to.fullPath}`) // 否则全部重定向到登录页
+      next({ path: '/login' ,query:{re_path:to.path,...to.query}})  // 否则全部重定向到登录页
       NProgress.done()
     }
   }
