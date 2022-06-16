c512bb53c3abce46ac1f8877eac66494b220e528
leebchi <chileeb@163.com>
2022-06-16 08:44:19 +0800

    GitEE 恢复

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysUserController.java
@@ -50,7 +50,7 @@ public class SysUserController extends BaseController
     private ISysPostService postService;
 
     /**
-     * 获取用户列表 Gitee
+     * 获取用户列表
      */
     @PreAuthorize("@ss.hasPermi('system:user:list')")
     @GetMapping("/list")
