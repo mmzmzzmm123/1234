0dd77b9bf48cc17d42884d4d9d7cab945326e7db
dengqingxia <2230676433@qq.com>
2022-08-01 09:40:07 +0800

    8月1号邓美女提交自己创建的一个包，里面是到时候用来写自己负责的业务代码，现在里面是空的

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,15 +1,20 @@
 package com.ruoyi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(
+        exclude = {DataSourceAutoConfiguration.class },//ruoyi使用了自定义的druid多数据源配置， 所以要排除默认的DaraSource自动配置类
+        scanBasePackages = {"com.ruoyi","com.zeroone"}//如果我们自己的业务代码不在com.ruoyi包下，需要添加包扫描并包含com.ruoyi
+        )
+@MapperScan("com.zeroone.crm.biz.mapper")//根据你的包名，可能需要额外指定mapper扫描的包名
 public class RuoYiApplication
 {
     public static void main(String[] args)
