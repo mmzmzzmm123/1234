fc39f497990b70e5a0cf73307ff5e001f1b402fb
小天才 <11328878+xtc99@user.noreply.gitee.com>
2022-08-04 14:56:19 +0800

    完成了一个课表的小功能

---
+++ course/src/main/java/com/ruoyi/course/service/impl/CourseServiceImpl.java
@@ -0,0 +1,93 @@
+package com.ruoyi.course.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ruoyi.course.mapper.CourseMapper;
+import com.ruoyi.course.domain.Course;
+import com.ruoyi.course.service.ICourseService;
+
+/**
+ * 课程信息Service业务层处理
+ * 
+ * @author ruoyi
+ * @date 2022-08-04
+ */
+@Service
+public class CourseServiceImpl implements ICourseService 
+{
+    @Autowired
+    private CourseMapper courseMapper;
+
+    /**
+     * 查询课程信息
+     * 
+     * @param id 课程信息主键
+     * @return 课程信息
+     */
+    @Override
+    public Course selectCourseById(Long id)
+    {
+        return courseMapper.selectCourseById(id);
+    }
+
+    /**
+     * 查询课程信息列表
+     * 
+     * @param course 课程信息
+     * @return 课程信息
+     */
+    @Override
+    public List<Course> selectCourseList(Course course)
+    {
+        return courseMapper.selectCourseList(course);
+    }
+
+    /**
+     * 新增课程信息
+     * 
+     * @param course 课程信息
+     * @return 结果
+     */
+    @Override
+    public int insertCourse(Course course)
+    {
+        return courseMapper.insertCourse(course);
+    }
+
+    /**
+     * 修改课程信息
+     * 
+     * @param course 课程信息
+     * @return 结果
+     */
+    @Override
+    public int updateCourse(Course course)
+    {
+        return courseMapper.updateCourse(course);
+    }
+
+    /**
+     * 批量删除课程信息
+     * 
+     * @param ids 需要删除的课程信息主键
+     * @return 结果
+     */
+    @Override
+    public int deleteCourseByIds(Long[] ids)
+    {
+        return courseMapper.deleteCourseByIds(ids);
+    }
+
+    /**
+     * 删除课程信息信息
+     * 
+     * @param id 课程信息主键
+     * @return 结果
+     */
+    @Override
+    public int deleteCourseById(Long id)
+    {
+        return courseMapper.deleteCourseById(id);
+    }
+}
