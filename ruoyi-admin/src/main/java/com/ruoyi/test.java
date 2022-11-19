117e9c9b1d0c50187387f176f4bd7a0843e23f83
Myq <460699822@qq.com>
2022-11-19 13:43:41 +0800

    测试3

--- ruoyi-admin/src/main/java/com/ruoyi/test.java
+++ ruoyi-admin/src/main/java/com/ruoyi/test.java
@@ -1,8 +1,9 @@
 package com.ruoyi;
 
 public class test {
-    public static void main(String[] sgra){
+    public static void main(String[] args){
         System.out.println("aabbcc");
         System.out.println("12345678");
+        System.out.println("test3");
     }
 }
