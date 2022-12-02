34e3d236371647b2f48075b58ed86f7c433b16e3
LBY <zhangshuda1998@163.com>
2022-12-02 08:49:14 +0000

    增加操作日志的部门id

---
+++ sql/update/20221202.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v11.25 (64 bit)
+MySQL - 8.0.26 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+DELETE FROM `sys_menu` WHERE menu_idBETWEEN 2013 AND 2014;
+
+insert into `sys_menu` (`menu_id`, `menu_name`, `parent_id`, `order_num`, `path`, `component`, `query`, `is_frame`, `is_cache`, `menu_type`, `visible`, `status`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) values('2013','部门管理','0','6','department',NULL,NULL,'1','0','M','0','0',NULL,'education','admin','2022-12-02 10:10:32','',NULL,'');
+insert into `sys_menu` (`menu_id`, `menu_name`, `parent_id`, `order_num`, `path`, `component`, `query`, `is_frame`, `is_cache`, `menu_type`, `visible`, `status`, `perms`, `icon`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) values('2014','修改日志','2013','1','department','department/dept',NULL,'1','0','C','0','0',NULL,'#','',NULL,'',NULL,'');
+
+
+
+
+DROP TABLE IF EXISTS `sys_oper_log` ;
+
+CREATE TABLE `sys_oper_log` (
+  `oper_id` bigint NOT NULL AUTO_INCREMENT COMMENT '日志主键',
+  `title` varchar(50) DEFAULT '' COMMENT '模块标题',
+  `business_type` int DEFAULT '0' COMMENT '业务类型（0其它 1新增 2修改 3删除）',
+  `method` varchar(100) DEFAULT '' COMMENT '方法名称',
+  `request_method` varchar(10) DEFAULT '' COMMENT '请求方式',
+  `operator_type` int DEFAULT '0' COMMENT '操作类别（0其它 1后台用户 2手机端用户）',
+  `oper_name` varchar(50) DEFAULT '' COMMENT '操作人员',
+  `dept_id` bigint DEFAULT NULL COMMENT '部门id',
+  `dept_name` varchar(50) DEFAULT '' COMMENT '部门名称',
+  `oper_url` varchar(255) DEFAULT '' COMMENT '请求URL',
+  `oper_ip` varchar(128) DEFAULT '' COMMENT '主机地址',
+  `oper_location` varchar(255) DEFAULT '' COMMENT '操作地点',
+  `oper_param` varchar(2000) DEFAULT '' COMMENT '请求参数',
+  `json_result` varchar(2000) DEFAULT '' COMMENT '返回参数',
+  `status` int DEFAULT '0' COMMENT '操作状态（0正常 1异常）',
+  `error_msg` varchar(2000) DEFAULT '' COMMENT '错误消息',
+  `oper_time` datetime DEFAULT NULL COMMENT '操作时间',
+  PRIMARY KEY (`oper_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=148 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='操作日志记录'
