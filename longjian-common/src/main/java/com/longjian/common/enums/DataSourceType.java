cb397301e30c696c85e5132264fc0c194dbc4362
yanglangjun <yanglangjun@fulan.com.cn>
2022-12-22 02:14:23 +0800

    去掉所有若依痕迹

---
+++ longjian-common/src/main/java/com/longjian/common/enums/DataSourceType.java
@@ -0,0 +1,19 @@
+package com.longjian.common.enums;
+
+/**
+ * 数据源
+ * 
+ * @author langjian
+ */
+public enum DataSourceType
+{
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE
+}
---
+++ longjian-ui/src/components/RuoYi/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'langjianDoc',
+  data() {
+    return {
+      url: 'http://doc.langjian.vip/langjian-vue'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
