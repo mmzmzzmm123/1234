97c632552d261b3dd5958b3c689187837cd71111
yangl4 <langjun.yang@metlife.com>
2022-12-22 16:03:06 +0800

    修复调整项目根目录之后打包报错问题

---
+++ langjian-admin/src/main/java/com/langjian/web/controller/monitor/ServerController.java
@@ -0,0 +1,27 @@
+package com.langjian.web.controller.monitor;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.langjian.common.core.domain.AjaxResult;
+import com.langjian.framework.web.domain.Server;
+
+/**
+ * 服务器监控
+ * 
+ * @author langjian
+ */
+@RestController
+@RequestMapping("/monitor/server")
+public class ServerController
+{
+    @PreAuthorize("@ss.hasPermi('monitor:server:list')")
+    @GetMapping()
+    public AjaxResult getInfo() throws Exception
+    {
+        Server server = new Server();
+        server.copyTo();
+        return AjaxResult.success(server);
+    }
+}
---
+++ langjian-common/src/main/java/com/langjian/common/core/domain/model/RegisterBody.java
@@ -0,0 +1,11 @@
+package com.langjian.common.core.domain.model;
+
+/**
+ * 用户注册对象
+ * 
+ * @author langjian
+ */
+public class RegisterBody extends LoginBody
+{
+
+}
