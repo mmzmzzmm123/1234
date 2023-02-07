cdd8a4ed424ed95b2e67654843bfd061dae271f5
yangchuang <chuangyangf@isoftstone.com>
2023-02-07 14:32:02 +0800

    微信支付

--- ruoyi-admin/src/main/java/com/ruoyi/app/controller/wechat/constant/WechatConstants.java
+++ ruoyi-admin/src/main/java/com/ruoyi/app/controller/wechat/constant/WechatConstants.java
@@ -2,23 +2,50 @@ package com.ruoyi.app.controller.wechat.constant;
 
 public class WechatConstants {
     //微信支付商户号
-    public static final String WECHAT_MCH_ID = "";
+    public static final String WECHAT_MCH_ID = "1634006526";
  
     //微信商户平台v2密钥
-    public static final String WECHAT_MCH_SECRET_V2 = "";
+    public static final String WECHAT_MCH_SECRET_V2 = "MIIEKDCCAxCgAwIBAgIUY9Kdfhtx5722";
  
     //微信商户平台v3密钥
-    public static final String WECHAT_MCH_SECRET_V3 = "";
+    public static final String WECHAT_MCH_SECRET_V3 = "AkNOMREwDwYDVQQHDAhTaGVuWmhlbjC3";
  
     //微信商户平台商户API证书序列号
-    public static final String WECHAT_MCH_SERIAL_NUM = "";
+    public static final String WECHAT_MCH_SERIAL_NUM = "63D29D7E1B71E7BE5B1D85B96C32B9E7D95FA400";
  
     //微信商户平台证书私钥 即证书中的apiclient_key.pem文件中的内容 可以直接写在这里 也可以用流读取文件
-    public static final String WECHAT_MCH_PRIVATE_KEY = "";
+    public static final String WECHAT_MCH_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\n" +
+            "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCbsajUx4MfyT/D\n" +
+            "C3Xg6RuCVPJzOyIYdQigkZ0ORmVCh52zUM8L1aKd3LMCnCTXTlzJGvxYEsly/Cx1\n" +
+            "3uechBKSobLZJDeoseXnRTLGSB8aoaS0AMIJK4zim7tzw+KxhwQaKSu0d9YekjQy\n" +
+            "lkQoSczOA4jQRTBILHc3gV4T9t88tuWTWimUUgdDOmZ6sTrdYqUt/9G4w7wTfuWK\n" +
+            "lRdk1dX7rKZ5el1dyu8y1PDSlg/pV4l2cF45IvE/nsZWtDPuTsUXFybmkbCziBC4\n" +
+            "aLAtxhfV72k7ix3HAVoc7lWS0E4Ie3C6Ay0ZU5rCAJQbLFOBYNnT11lUEpPuc3pq\n" +
+            "D0CFqBtVAgMBAAECggEASb9ujFNw3eaS6E2OaZmhaSvZqBFzhggxrPUsYq2fm1Ln\n" +
+            "tR9q410vaP04Yd0aak35D11T+Ff1yhx+5tpN9UpuDbzAHbDVGHgjwVd6xIc3DINS\n" +
+            "o9zUDEnvsws3UI+R2qtsmwOzawl0iAiS5v9ci4ThmRf9f0G2rAk78zO5H1XJylTO\n" +
+            "4LmYMRXTdT9FFRM93Vk4N6FF+vfMFbgzBhXvNEg3JoOV6Wzmi14JVhkTTP7a4HqJ\n" +
+            "Oo6YWbOwGjshRDhTGSHNo88si+DnAd2mzDhki1qw/xcfuIvw9JCpdDJ2QDeEP9eT\n" +
+            "XOPx022WjhQO3rnN2Gb6yzP3rHxlSY9XkJ+YreqvRQKBgQDLgaEiokvnlCRIzSvQ\n" +
+            "cC23WTypjvI713sr0ffa/J7JTz94Ah9lRJSFiTfiafBTflGEcygcMH91aM5e/plA\n" +
+            "djvEKbJCmwMgusfFd0pjy9WMV98Do1w8JIxj4etQbsK3TFyw50gbV6iyv+C9ttrO\n" +
+            "kBuT12tJDIoc55rFQhS0h2mJOwKBgQDD2sekDfSBJnNGfM5AG7idXrlt2GjVlS7X\n" +
+            "SG+J/VO6Sr9UtKupoS+17zpmC4quDLhOSIfXFsw5Q8h3YYa7WNQd4mlAVdYzpxI1\n" +
+            "4VLwQvi7HDmfkA4Dsn3RjPnHuP0y7EgnZ7ptr9OlDoOUsVpUipOqCFcozzS7B159\n" +
+            "2sW/3DUkrwKBgQDJmyEj636g68zKZp+6Pfe8ROZo39kv2XgHYFpYEp3Htv+chxXN\n" +
+            "QBSW+epoHiVcwa5GNcMNsioDCZIw3665AqiQ2/HiNCj0GrXf9R64IO3su/yOYR71\n" +
+            "4gtGxEBXjXLPHhJRmr5/f+b7NkWSDH1V18PKQcXiN0739aYJNz7F3cR6kwKBgQC2\n" +
+            "PsNcpdupvdSwd3wYbXMphLY5P5G1hN8UVoPrvD2H8Pv+3yuJacKSoewOfpjqAk4S\n" +
+            "Ei3M+JrKgodshuSvOiYPNNpJklYCtGkel+/sP/Vhbw8zqgFQcWvgbTUUItAA7Zrq\n" +
+            "h/wTCvR+93V7DFkIp/Zl1uL7x2vCyUWlCMjQEiGlYwKBgHUM+c5gIrcozF+ec+eb\n" +
+            "FSmCqmIiLRZkecMm7JbEvk1IhCNQG3LfjmipEeUQdEyMQt33GejGg9lcdYcWxr62\n" +
+            "noR7dG34ndLGV/d7wTxgIcqfVxQ/ZCz/bT5SYMZhExq7r3zy3uUqeeh6MSukMJA5\n" +
+            "My4+WvMs9HW82I2pYHWJVpJ2\n" +
+            "-----END PRIVATE KEY-----\n";
  
     //微信小程序appid
-    public static final String WECHAT_MP_APPID = "";
+    public static final String WECHAT_MP_APPID = "wx81f97b38440a3b86";
  
     //微信小程序密钥
-    public static final String WECHAT_MP_SECRET = "";
+    public static final String WECHAT_MP_SECRET = "f259b9c0951de86bcfda3e482e7b3c45";
 }
