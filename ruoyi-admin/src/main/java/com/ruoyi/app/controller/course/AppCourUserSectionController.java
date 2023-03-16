f57235e4d4c911104e0cb55a695b4a7bbafd1884
zjluq <zjluq@isoftstone.com>
2023-03-16 15:20:59 +0800

    fix: 新增或更新用户-课程-章节关系,记录课程章节完成情况

--- ruoyi-admin/src/main/java/com/ruoyi/app/controller/course/AppCourUserSectionController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/app/controller/course/AppCourUserSectionController.java
@@ -1,6 +1,7 @@
 package com.ruoyi.app.controller.course;
 
 import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.course.domain.CourCourse;
 import com.ruoyi.course.domain.CourUserCourseSection;
@@ -10,10 +11,7 @@ import com.ruoyi.course.vo.CourseVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,9 +31,9 @@ public class AppCourUserSectionController extends BaseController {
      * 查询用户的课程列表
      */
 //    @PreAuthorize("@ss.hasPermi('course:userSection:list')")
-    @GetMapping("/list")
+    @PostMapping("/list")
     @ApiOperation("查询课程章节列表")
-    public TableDataInfo list(CourUserCourseSection courUserCourseSection)
+    public TableDataInfo list(@RequestBody CourUserCourseSection courUserCourseSection)
     {
         startPage();
         List<CourUserCourseSection> list = courUserCourseSectionService.selectCourUserCourseSectionList(courUserCourseSection);
@@ -74,32 +72,23 @@ public class AppCourUserSectionController extends BaseController {
     }
 
     /**
-     * 查询用户的课程列表
+     * 新增或更新用户-课程-章节关系
      */
-    @PostMapping("/getSectionListFinish")
-    @ApiOperation("根据用户编号和课程编号查询章节列表完成信息")
-    public TableDataInfo getSectionList(CourUserCourseSection courUserCourseSection)
+    @PostMapping("/saveUserSectionInfo")
+    @ApiOperation("记录课程章节完成情况，新增或更新用户-课程-章节关系")
+    public AjaxResult saveUserSectionInfo(@RequestBody CourUserCourseSection courUserCourseSection)
     {
-        startPage();
-        List<CourCourse> list = courUserCourseSectionService.getCourseListByUserId(courUserCourseSection);
-
-        List<CourseVO> courseVOList = new ArrayList<>();
-        list.forEach(courCourse -> {
-            boolean hasUnFinished = courCourseService.calCourCourseList(courCourse.getCourseId());
-
-            CourseVO courseVO = new CourseVO();
-            courseVO.setId(courCourse.getId());
-            courseVO.setCourseId(courCourse.getCourseId());
-            courseVO.setName(courCourse.getName());
-            courseVO.setType(courCourse.getType());
-            courseVO.setAuthor(courCourse.getAuthor());
-            courseVO.setUrl(courCourse.getUrl());
-            courseVO.setPrice(courCourse.getPrice());
-            courseVO.setDetail(courCourse.getDetail());
-            courseVO.setFinishStatus(hasUnFinished ? 0 : 1);
-
-            courseVOList.add(courseVO);
-        });
-        return getDataTable(courseVOList);
+        CourUserCourseSection userCourseSection = new CourUserCourseSection();
+        userCourseSection.setUserId(courUserCourseSection.getUserId());
+        userCourseSection.setCourseId(courUserCourseSection.getCourseId());
+        userCourseSection.setSectionId(courUserCourseSection.getSectionId());
+        // 根据用户、课程、章节查询是否已有学习完成记录
+        if (this.list(userCourseSection).getTotal() == 0) {
+            // 新增
+            return AjaxResult.success(courUserCourseSectionService.insertCourUserCourseSection(courUserCourseSection));
+        } else {
+            // 更新
+            return AjaxResult.success(courUserCourseSectionService.updateCourUserCourseSection(courUserCourseSection));
+        }
     }
 }
