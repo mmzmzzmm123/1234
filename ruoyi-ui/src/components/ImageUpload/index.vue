48598e347190463ea4a544b7aa63a5f83b81f0b8
zjluq <zjluq@isoftstone.com>
2023-04-03 15:00:34 +0800

    fix: 图片上传组件的回调url和显示url兼容使用云存储路径

--- ruoyi-ui/src/components/ImageUpload/index.vue
+++ ruoyi-ui/src/components/ImageUpload/index.vue
@@ -97,7 +97,7 @@ export default {
           // 然后将数组转为对象数组
           this.fileList = list.map(item => {
             if (typeof item === "string") {
-              if (item.indexOf(this.baseUrl) === -1) {
+              if (item.indexOf(this.baseUrl) === -1 && item.indexOf('http') === -1) {
                   item = { name: this.baseUrl + item, url: this.baseUrl + item };
               } else {
                   item = { name: item, url: item };
@@ -159,7 +159,7 @@ export default {
     // 上传成功回调
     handleUploadSuccess(res, file) {
       if (res.code === 200) {
-        this.uploadList.push({ name: res.fileName, url: res.fileName });
+        this.uploadList.push({ name: res.originalFilename, url: res.url });
         this.uploadedSuccessfully();
       } else {
         this.number--;
