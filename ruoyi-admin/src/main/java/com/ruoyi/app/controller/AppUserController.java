2138ecec593124089e8dfe18536d0c90c415df71
zhang zhan <270391484@qq.com>
2023-04-18 17:36:35 +0800

    新增微信小程序端的企业入驻接口

--- ruoyi-admin/src/main/java/com/ruoyi/app/controller/AppUserController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/app/controller/AppUserController.java
@@ -8,6 +8,7 @@ import com.ruoyi.system.biz.token.TokenAuthorize;
 import com.ruoyi.system.biz.token.WechatTokenServiceImpl;
 import com.ruoyi.system.domain.DataWechatUser;
 import com.ruoyi.system.service.IDataWechatUserService;
+import com.ruoyi.system.service.impl.ProdOpenApi;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -33,6 +34,9 @@ public class AppUserController extends BaseController {
 
     @Autowired
     private IDataWechatUserService userService;
+
+    @Autowired
+    private ProdOpenApi prodOpenApi;
 
     /**
      * 登录
@@ -97,6 +101,37 @@ public class AppUserController extends BaseController {
         long userId = (long) request.getAttribute("userId");
         pbaUser.setUserId(userId);
         return toAjax(userService.updateDataWechatUser(pbaUser));
+    }
+
+    /**
+     * 微信小程序企业入驻
+     * @return
+     */
+    @TokenAuthorize
+    @PostMapping("/comUserReg")
+    public AjaxResult comUserReg() {
+
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+        long userId = (long) request.getAttribute("userId");
+        DataWechatUser user = userService.selectDataWechatUserById(userId);
+
+        if (user != null){
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("USER_NAME",user.getCompanyName());
+            jsonObject.put("USER_IDCARD",user.getTyshxydm());
+            jsonObject.put("LEGALPERSON_IDCARD",user.getLegalPersonIdcard());
+            jsonObject.put("LEGALPERSON_NAME",user.getLegalPersonName());
+            jsonObject.put("USER_MOBILE",user.getLegalPersonPhone());
+            jsonObject.put("LEGALPERSON_JBRIDCARD",user.getJbrIdcard());
+            jsonObject.put("LEGALPERSON_JBRNAME",user.getJbrName());
+            jsonObject.put("USER_TYPE",2);// 设置为法人用户
+            // jsonObject.put("",user.getJbrPhone()); // 入驻接口不要求经办人手机号码
+
+            return prodOpenApi.comUserReg(jsonObject);
+        }else {
+            return AjaxResult.error("获取用户信息失败");
+        }
     }
 
 }
