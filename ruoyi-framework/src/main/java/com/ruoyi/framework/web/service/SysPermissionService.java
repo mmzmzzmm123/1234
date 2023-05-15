b1da2a5306cff74708d71ca383101a830bc88a8e
huyunfeng <302976975@qq.com>
2023-05-15 15:23:19 +0800

    在对接小程序项目时，出现roles为null的情况。所以，这里防止roles为null，报空指针异常。

--- ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysPermissionService.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/web/service/SysPermissionService.java
@@ -9,10 +9,11 @@ import com.ruoyi.common.core.domain.entity.SysRole;
 import com.ruoyi.common.core.domain.entity.SysUser;
 import com.ruoyi.system.service.ISysMenuService;
 import com.ruoyi.system.service.ISysRoleService;
+import org.springframework.util.CollectionUtils;
 
 /**
  * 用户权限处理
- * 
+ *
  * @author ruoyi
  */
 @Component
@@ -26,7 +27,7 @@ public class SysPermissionService
 
     /**
      * 获取角色数据权限
-     * 
+     *
      * @param user 用户信息
      * @return 角色权限信息
      */
@@ -47,7 +48,7 @@ public class SysPermissionService
 
     /**
      * 获取菜单数据权限
-     * 
+     *
      * @param user 用户信息
      * @return 菜单权限信息
      */
@@ -62,7 +63,7 @@ public class SysPermissionService
         else
         {
             List<SysRole> roles = user.getRoles();
-            if (!roles.isEmpty() && roles.size() > 1)
+            if (!CollectionUtils.isEmpty(roles))
             {
                 // 多角色设置permissions属性，以便数据权限匹配权限
                 for (SysRole role : roles)
