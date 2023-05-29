5ae8ff4f029d319e30df752335385ea353e8e33e
DESKTOP-QOE10KP\18600 <18600992571@163.com>
2023-05-29 10:47:21 +0800

    初始化代码  mp改造

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/BaseEntity.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/BaseEntity.java
@@ -4,21 +4,26 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
 
 /**
  * Entity基类
  * 
  * @author ruoyi
  */
+@Data
 public class BaseEntity implements Serializable
 {
     private static final long serialVersionUID = 1L;
 
     /** 搜索值 */
     @JsonIgnore
+    @TableField(exist = false)
     private String searchValue;
 
     /** 创建者 */
@@ -39,6 +44,7 @@ public class BaseEntity implements Serializable
     private String remark;
 
     /** 请求参数 */
+    @TableField(exist = false)
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Map<String, Object> params;
 
