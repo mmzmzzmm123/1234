42acf26ce35c5ccbc04bbc907d50a357be6d17a4
hanbin <592633221@qq.com>
2023-09-17 11:52:10 +0800

    feat 添加房间收费类型字段

--- unattended-office/src/main/java/com/ruoyi/office/service/impl/TRoomPackageServiceImpl.java
+++ unattended-office/src/main/java/com/ruoyi/office/service/impl/TRoomPackageServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ruoyi.office.service.impl;
 
 import java.util.List;
+
 import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.office.domain.TRoom;
+import com.ruoyi.office.mapper.TRoomMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,87 +14,92 @@ import com.ruoyi.office.service.ITRoomPackageService;
 
 /**
  * 房间套餐价格Service业务层处理
- * 
+ *
  * @author ruoyi
  * @date 2023-09-14
  */
 @Service
-public class TRoomPackageServiceImpl extends ServiceImpl<TRoomPackageMapper, TRoomPackage> implements ITRoomPackageService
-{
+public class TRoomPackageServiceImpl extends ServiceImpl<TRoomPackageMapper, TRoomPackage> implements ITRoomPackageService {
     @Autowired
     private TRoomPackageMapper tRoomPackageMapper;
+    @Autowired
+    private TRoomMapper tRoomMapper;
 
     /**
      * 查询房间套餐价格
-     * 
+     *
      * @param id 房间套餐价格主键
      * @return 房间套餐价格
      */
     @Override
-    public TRoomPackage selectTRoomPackageById(Long id)
-    {
+    public TRoomPackage selectTRoomPackageById(Long id) {
         return tRoomPackageMapper.selectTRoomPackageById(id);
     }
 
     /**
      * 查询房间套餐价格列表
-     * 
+     *
      * @param tRoomPackage 房间套餐价格
      * @return 房间套餐价格
      */
     @Override
-    public List<TRoomPackage> selectTRoomPackageList(TRoomPackage tRoomPackage)
-    {
+    public List<TRoomPackage> selectTRoomPackageList(TRoomPackage tRoomPackage) {
         return tRoomPackageMapper.selectTRoomPackageList(tRoomPackage);
     }
 
     /**
      * 新增房间套餐价格
-     * 
+     *
      * @param tRoomPackage 房间套餐价格
      * @return 结果
      */
     @Override
-    public int insertTRoomPackage(TRoomPackage tRoomPackage)
-    {
+    public int insertTRoomPackage(TRoomPackage tRoomPackage) {
         tRoomPackage.setCreateTime(DateUtils.getNowDate());
-        return tRoomPackageMapper.insertTRoomPackage(tRoomPackage);
+        int res = tRoomPackageMapper.insertTRoomPackage(tRoomPackage);
+        TRoom tRoom = new TRoom();
+        tRoom.setId(tRoomPackage.getRoomId());
+        tRoom.setRoomPayType("1");
+        tRoomMapper.updateTRoom(tRoom);
+        return res;
     }
 
     /**
      * 修改房间套餐价格
-     * 
+     *
      * @param tRoomPackage 房间套餐价格
      * @return 结果
      */
     @Override
-    public int updateTRoomPackage(TRoomPackage tRoomPackage)
-    {
+    public int updateTRoomPackage(TRoomPackage tRoomPackage) {
         tRoomPackage.setUpdateTime(DateUtils.getNowDate());
-        return tRoomPackageMapper.updateTRoomPackage(tRoomPackage);
+        int res = tRoomPackageMapper.updateTRoomPackage(tRoomPackage);
+        TRoom tRoom = new TRoom();
+        tRoom.setId(tRoomPackage.getRoomId());
+        tRoom.setRoomPayType("1");
+        tRoomMapper.updateTRoom(tRoom);
+        return res;
     }
 
     /**
      * 批量删除房间套餐价格
-     * 
+     *
      * @param ids 需要删除的房间套餐价格主键
      * @return 结果
      */
     @Override
-    public int deleteTRoomPackageByIds(Long[] ids)
-    {
+    public int deleteTRoomPackageByIds(Long[] ids) {
         return tRoomPackageMapper.deleteTRoomPackageByIds(ids);
     }
 
     /**
      * 删除房间套餐价格信息
-     * 
+     *
      * @param id 房间套餐价格主键
      * @return 结果
      */
     @Override
-    public int deleteTRoomPackageById(Long id)
-    {
+    public int deleteTRoomPackageById(Long id) {
         return tRoomPackageMapper.deleteTRoomPackageById(id);
     }
 }
