5929708720f6ec99697824c92796dcdd731da3d4
qinjin <qinjin@71info.com>
2023-09-20 14:42:20 +0800

    add: 代码提交

--- ruoyi-ui/src/views/components/gauge/setting/index.vue
+++ ruoyi-ui/src/views/components/gauge/setting/index.vue
@@ -8,7 +8,7 @@
           <el-form ref="form" label-width="80px">
             <el-form-item :label="gaugeType === 3 ? '性格类型' : '得分范围'">
               <template v-if="gaugeType === 3">
-                <el-select v-if="gaugeType === 3" @change="modifySetting(props.row)" size="small" style="width: 150px" v-model="props.row.lat" placeholder="纬度" clearable>
+                <el-select v-if="gaugeType === 3" size="small" style="width: 150px" v-model="props.row.lat" placeholder="纬度" clearable>
                   <el-option
                     v-for="item in gaugeMbtiRes"
                     :key="item.value"
@@ -19,11 +19,11 @@
               </template>
               <template v-else>
                 <el-col :span="7">
-                  <el-input-number style="width: 100%;" controls-position="right" size="small" v-model="props.row.start" @change="modifySetting(props.row)"></el-input-number>
+                  <el-input-number style="width: 100%;" controls-position="right" size="small" v-model="props.row.start"/>
                 </el-col>
                 <el-col class="line" :span="1">-</el-col>
                 <el-col :span="7">
-                  <el-input-number style="width: 100%;" controls-position="right" size="small" v-model="props.row.end" @change="modifySetting(props.row)"></el-input-number>
+                  <el-input-number style="width: 100%;" controls-position="right" size="small" v-model="props.row.end"/>
                 </el-col>
               </template>
             </el-form-item>
@@ -31,8 +31,10 @@
               <image-upload v-model="props.row.proposal" @input="modifySetting(props.row)" :extraData="extraData"/>
             </el-form-item>
             <el-form-item label="结论">
-              <editor v-model="props.row.result" placeholder="请输入内容" @blur="modifySetting(props.row)"
-                      :min-height="192"/>
+              <editor v-model="props.row.result" placeholder="请输入内容" :min-height="192"/>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="modifySetting(props.row)">确 定</el-button>
             </el-form-item>
 
           </el-form>
@@ -48,7 +50,7 @@
           </span>
         </template>
       </el-table-column>
-      <el-table-column label="建议" prop="proposal">
+      <el-table-column label="描述" prop="proposal">
         <template slot-scope="props" v-if="props.row.proposal!=null">
           <image-preview v-for="url in props.row.proposal.split(',')" :key="url" :src="url" :width="50" :height="50"/>
         </template>
@@ -135,6 +137,7 @@ export default {
     },
     async modifySetting(data) {
       await updateSetting(data);
+      this.$modal.msgSuccess("保存成功");
     },
     handleDelete(data) {
       this.$modal
