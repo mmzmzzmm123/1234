e06f717dd854967ea00eaa1b56a7ad4ac417e41a
DESKTOP-QOE10KP\18600 <18600992571@163.com>
2023-09-22 09:35:10 +0800

    商户开设备

---
+++ unattended-office/src/main/java/com/ruoyi/office/domain/vo/RoomEquipeOpenReq.java
@@ -0,0 +1,11 @@
+package com.ruoyi.office.domain.vo;
+
+import lombok.Data;
+
+@Data
+public class RoomEquipeOpenReq {
+
+    private Long roomId;
+    private String equipType;//门禁	door    电灯	light    空调	aircondition
+}
+
