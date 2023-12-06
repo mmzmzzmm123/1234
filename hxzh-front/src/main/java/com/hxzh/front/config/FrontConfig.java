0cca1835d7f9873d12569d9e52585e14b084b299
jasonyou <2926593225@qq.com>
2023-12-07 04:09:04 +0800

    设定组件扫描和mapper扫描范围

---
+++ hxzh-front/src/main/java/com/hxzh/front/config/FrontConfig.java
@@ -0,0 +1,9 @@
+package com.hxzh.front.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+//扫描com.ruoyi下的所有类
+@Component
+public class FrontConfig {
+}
