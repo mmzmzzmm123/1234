2d16e56e4d93ae2aa9e10dbba7c55ff2824f41b2
Jing.Zhang <784823787@qq.com>
2023-12-27 10:47:10 +0800

    :art: feat(config): 增加Config模块跨域请求;

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -62,8 +62,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     /**
      * 跨域过滤器
      */
-//    @Autowired
-//    private CorsFilter corsFilter;
+    @Autowired
+    private CorsFilter corsFilter;
 
     /**
      * 允许匿名访问的地址
@@ -133,8 +133,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
         // 添加JWT filter
         httpSecurity.addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
         // 添加CORS filter
-//        httpSecurity.addFilterBefore(corsFilter, JwtAuthenticationTokenFilter.class);
-//        httpSecurity.addFilterBefore(corsFilter, LogoutFilter.class);
+        httpSecurity.addFilterBefore(corsFilter, JwtAuthenticationTokenFilter.class);
+        httpSecurity.addFilterBefore(corsFilter, LogoutFilter.class);
     }
 
     /**
