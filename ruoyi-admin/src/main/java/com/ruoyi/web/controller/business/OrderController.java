79847e56626a2fe2aab55c3592013ec2c810db54
zengyi <17610895897@163.com>
2023-12-30 21:02:01 +0800

    u

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/business/OrderController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/business/OrderController.java
@@ -8,12 +8,14 @@ import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.domain.R;
 import com.ruoyi.common.core.domain.app.*;
+import com.ruoyi.common.core.domain.model.LoginUser;
 import com.ruoyi.common.exception.ServiceException;
 import com.ruoyi.common.utils.Ids;
 import com.ruoyi.system.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,7 +41,12 @@ public class OrderController extends BaseController {
 	@ApiOperation("下单(返回订单id)")
 	@PostMapping("produce")
 	public R<String> produce(@RequestBody OrderProduceRequest dto) {
-		dto.setLoginUser(getLoginUser());
+		LoginUser loginUser = getLoginUser();
+		if (ObjectUtils.isEmpty(loginUser.getMerchantInfo()) || loginUser.getMerchantInfo().getMerchantType() != 0) {
+			return R.fail(ErrInfoConfig.getDynmic(11011));
+		}
+
+		dto.setLoginUser(loginUser);
 		try {
 			AjaxResult r = orderService.produce(dto);
 			if (r.isSuccess()) {
