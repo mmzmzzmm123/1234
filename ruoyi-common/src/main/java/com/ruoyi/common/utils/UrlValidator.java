379f44db310d9989a5a9fc17e4063e5b88147d4c
zengyi <17610895897@163.com>
2023-12-30 21:54:24 +0800

    修改url验证规则

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/UrlValidator.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/UrlValidator.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
  */
 public class UrlValidator {
     public static boolean validate(String url) {
-        String regex = "^(http|https):\\/\\/.+";
+        String regex = "^http(s)?\\:\\/\\/t\\.me\\/[a-zA-Z+\\-0-9]+$";
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(url);
