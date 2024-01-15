3fafa3a8a1f7abaf46c625ede449499af6016c6a
Xiao <205501564@qq.com>
2024-01-15 17:13:46 +0800

    氛围号池

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/vo/robot/GetRobotVO.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/vo/robot/GetRobotVO.java
@@ -9,8 +9,4 @@ import lombok.Data;
 public class GetRobotVO {
     @ApiModelProperty(value = "机器号编号")
     private String robotSerialNo;
-    @ApiModelProperty(value = "每日设为群管理员可用次数")
-    private Integer oneDaySetAdminCount;
-    @ApiModelProperty(value = "单个水军可带群数")
-    private Integer groupCount;
 }
