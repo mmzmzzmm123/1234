3bf5c4e184bb09864404b170b852175dd2e3d318
我不是王大锤 <onlybigroc@qq.com>
2024-01-15 22:27:23 +0800

    feat:o#台账

---
+++ ruoyi-ui/src/api/base/bill.js
@@ -0,0 +1,52 @@
+import request from '@/utils/request'
+
+// 查询原数据---0号台账列表
+export function listBill(query) {
+  return request({
+    url: '/source/zeroBill/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询原数据---0号台账详细
+export function getBill(id) {
+  return request({
+    url: '/source/zeroBill/' + id,
+    method: 'get'
+  })
+}
+
+// 新增原数据---0号台账
+export function addBill(data) {
+  return request({
+    url: '/source/zeroBill/saveOrUpdate',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改原数据---0号台账
+export function updateBill(data) {
+  return request({
+    url: '/source/zeroBill/saveOrUpdate',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除原数据---0号台账
+export function delBill(id) {
+  return request({
+    url: '/source/zeroBill/' + id,
+    method: 'delete'
+  })
+}
+// 导入表
+export function importTable(data) {
+  return request({
+    url: '/source/zeroBill/import',
+    method: 'post',
+    params: data
+  })
+}
