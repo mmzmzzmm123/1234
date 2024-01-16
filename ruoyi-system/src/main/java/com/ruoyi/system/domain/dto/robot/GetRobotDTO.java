f068976666b899a07ab381d4c8956312c5c12d90
Xiao <205501564@qq.com>
2024-01-16 11:16:41 +0800

    氛围号池

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/dto/robot/GetRobotDTO.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/dto/robot/GetRobotDTO.java
@@ -10,11 +10,11 @@ import java.util.List;
 @ApiModel("GetRobotDTO")
 public class GetRobotDTO {
     @ApiModelProperty(value = "需要的号数量")
-    private Integer count;
+    private Integer count = 0;
+    @ApiModelProperty(value = "需要的设置管理员的号数量")
+    private Integer setAdminCount = 0;
     @ApiModelProperty(value = "国家编码")
     private List<String> countryCode;
-    @ApiModelProperty(value = "是否需要设置管理员,0-否,1-是")
-    private Integer isSetAdmin;
     @ApiModelProperty(value = "是否需要离散,0-否,1-B段离散,2-C段离散")
     private Integer ipType;
 }
