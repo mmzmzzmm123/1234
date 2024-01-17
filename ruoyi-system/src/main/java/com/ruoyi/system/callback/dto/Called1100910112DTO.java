f93ca6211ed8c6f7bacb2839a429a6c6bf2cb19b
luoyilin <443426743@qq.com>
2024-01-17 16:25:58 +0800

    回调以及openapi修改

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/callback/dto/Called1100910112DTO.java
@@ -0,0 +1,15 @@
+package com.ruoyi.system.callback.dto;
+
+import lombok.Data;
+
+@Data
+public class Called1100910112DTO {
+
+        private String new_chatroom_serial_no;
+
+        private Boolean is_active;
+
+        private String robot_serial_no;
+
+        private String old_chatroom_serial_no;
+}
