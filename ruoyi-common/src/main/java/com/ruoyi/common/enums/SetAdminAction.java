740205263f3867ef62856c89bf9774ef9e9aac01
zhushaobo <zhushaobo1990@outlook.com>
2024-01-18 23:30:15 +0800

    增加群封禁逻辑

---
+++ ruoyi-common/src/main/java/com/ruoyi/common/enums/SetAdminAction.java
@@ -0,0 +1,46 @@
+package com.ruoyi.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.EnumSet;
+
+/**
+ * @author 朱少波
+ * @date 2024/1/17/017 9:30
+ */
+@AllArgsConstructor
+@Getter
+public enum SetAdminAction {
+
+
+    QUERY_HASH(1, GroupAction.QUERY_HASH, 5),
+
+    SET_BOT_ADMIN(2, GroupAction.SET_GROUP_ADMIN, 0);
+
+    private final int code;
+
+    private final GroupAction action;
+
+    private final int retryCount;
+
+
+    public static SetAdminAction of(Integer code) {
+        if (code == null) {
+            return null;
+        }
+        return EnumSet.allOf(SetAdminAction.class)
+                .stream()
+                .filter(p -> p.getCode() == code)
+                .findAny()
+                .orElse(null);
+    }
+
+    public SetAdminAction getNextAction() {
+        return EnumSet.allOf(SetAdminAction.class)
+                .stream()
+                .filter(p -> p.getCode() == code + 1)
+                .findAny()
+                .orElse(null);
+    }
+}
