250a3fa4763f1c4d018ee2fe0c0f2d770cc88979
zengyi <17610895897@163.com>
2024-01-18 23:38:03 +0800

    u

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/dto/play/PlayStateDTO.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/dto/play/PlayStateDTO.java
@@ -12,7 +12,7 @@ public class PlayStateDTO {
     @NotNull(message = "炒群任务ID不能为空")
     private List<String> playIds;
 
-    @ApiModelProperty("状态: 3暂停 4取消")
+    @ApiModelProperty("状态: 2开启 3暂停 4取消")
     @NotNull(message = "操作类型不能为空")
     private Integer state;
 }
