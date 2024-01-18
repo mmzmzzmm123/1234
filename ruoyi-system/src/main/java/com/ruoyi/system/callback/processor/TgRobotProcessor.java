1e7362da8eb14632c2f84d88cf1e849c79ca6703
luoyilin <443426743@qq.com>
2024-01-18 19:32:19 +0800

    获取群信息

--- ruoyi-system/src/main/java/com/ruoyi/system/callback/processor/TgRobotProcessor.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/callback/processor/TgRobotProcessor.java
@@ -325,10 +325,20 @@ public class TgRobotProcessor {
      * @param dto
      */
     @Type(value = 1100910083, parameterClass = CalledEmptyDTO.class)
-    public void called1100910083(Called1100910011DTO dto) {
+    public void called1100910083(CalledEmptyDTO dto) {
         CalledDTO root = CalledDTOThreadLocal.getAndRemove();
     }
 
+
+    /**
+     * TG 设置隐私手机号码谁可见接口
+     *
+     * @param dto
+     */
+    @Type(value = 1100910043, parameterClass = CalledEmptyDTO.class)
+    public void called1100910043(CalledEmptyDTO dto) {
+        CalledDTO root = CalledDTOThreadLocal.getAndRemove();
+    }
 
 
 }
