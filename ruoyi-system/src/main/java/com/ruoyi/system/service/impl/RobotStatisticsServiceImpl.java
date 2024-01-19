12cfa8091c6fb410925cbd189e3e4ef0e88ee75a
Xiao <205501564@qq.com>
2024-01-20 04:01:07 +0800

    氛围号池

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/RobotStatisticsServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/RobotStatisticsServiceImpl.java
@@ -79,7 +79,7 @@ public class RobotStatisticsServiceImpl extends ServiceImpl<RobotStatisticsMappe
         //如果有需要设置管理员号的,需要再次获取其他号
         if(dto.getCount() > 0){
             List<String> ips = selectRobotByRuleVOS.stream().filter(f->StringUtils.isNotEmpty(f.getIp())).map(SelectRobotByRuleVO::getIp).collect(Collectors.toList());
-            if(CollectionUtils.isEmpty(ips)){
+            if(!CollectionUtils.isEmpty(ips)){
                 selectRobotByRuleDTO.setIps(ips);
             }
             List<String> excludeRobotSerialNos = selectRobotByRuleVOS.stream().map(SelectRobotByRuleVO::getRobotSerialNo).collect(Collectors.toList());
