65c5a20deaf0528f7bceedbe2ff37226bf24838e
Jing.Zhang <784823787@qq.com>
2024-01-28 14:40:29 +0800

    :bug: fix(admin): 启动时清理OpenApiSdk缓存信息;

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -3,8 +3,10 @@ package com.ruoyi;
 import com.alibaba.fastjson2.support.spring.http.converter.FastJsonHttpMessageConverter;
 import com.ruoyi.common.config.dymic.EnableRedisConfigure;
 import com.ruoyi.common.core.delayqueue.EnableRedissonFastDelayQueue;
+import com.ruoyi.common.utils.spring.SpringUtils;
 import com.ruoyi.system.service.impl.IntoGroupService;
 import com.ruoyi.system.service.limit.WarningRobotLimitService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -12,28 +14,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableRedisConfigure
 @Import(FastJsonHttpMessageConverter.class)
 @EnableScheduling
 @EnableRedissonFastDelayQueue
-public class RuoYiApplication implements ApplicationRunner
-{
+public class RuoYiApplication implements ApplicationRunner {
     @Autowired
     IntoGroupService intoGroupService;
 
     @Autowired
     WarningRobotLimitService warningRobotLimitService;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
@@ -50,5 +54,12 @@ public class RuoYiApplication implements ApplicationRunner
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        RedisTemplate<String, String> redisTemplate = SpringUtils.getBean("redisTemplate");
+        BoundValueOperations<String, String> valueOperations =
+                redisTemplate.boundValueOps("ut-buleprint-backend:OpenApiToken");
+        String token = valueOperations.get();
+        if (StringUtils.isBlank(token)) {
+            valueOperations.expire(1, TimeUnit.MILLISECONDS);
+        }
     }
 }
