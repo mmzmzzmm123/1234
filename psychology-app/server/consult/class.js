cb96758818fafa51a15000ed27415b7c211b2172
qinjin <qinjin@71info.com>
2024-04-12 11:10:07 +0800

    edit: 金刚位状态问题修改

--- psychology-app/server/consult/class.js
+++ psychology-app/server/consult/class.js
@@ -2,7 +2,7 @@ import httprequest from "../httpRequest";
 export default {
   //根据字典类型获取banner图
   getClassList: async (bannerType) => {
-    let res = await httprequest.get("/app/consult/class/list");
+    let res = await httprequest.get("/app/consult/class/list?status=0");
     if (res.code == 200) {
       return res.rows;
     } else {
