0f4516d412b2eff2f9b9728092cc1683bdf32515
xhlvk <xhlvk@isoftstone.com>
2024-06-20 18:11:40 +0800

    注释

--- sql/update/update.sql
+++ sql/update/update.sql
@@ -1,2 +1,4 @@
-ALTER TABLE psy_consult_work ADD COLUMN consult_type CHAR(255) NOT NULL DEFAULT '1' COMMENT '1 咨询 2倾听 3督导';
+ALTER TABLE psy_consult_work ADD COLUMN consult_type CHAR(4) NOT NULL DEFAULT '1' COMMENT '1 咨询 2倾听 3督导'  AFTER consult_id;
 update psy_consult_work set consult_type = '1' where consult_type is null;
+
+ALTER TABLE psy_consult_order ADD COLUMN consult_type CHAR(4) NOT NULL DEFAULT '1' COMMENT '1 咨询 2倾听 3督导' AFTER nick_name;
