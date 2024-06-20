263c8483df2781132790dc4a94d3fe9a9a3cc4ac
xhlvk <xhlvk@isoftstone.com>
2024-06-20 16:33:04 +0800

    注释

--- ruoyi-framework/src/main/java/com/renxin/framework/security/filter/JwtAuthenticationTokenFilter.java
+++ ruoyi-framework/src/main/java/com/renxin/framework/security/filter/JwtAuthenticationTokenFilter.java
@@ -44,26 +44,39 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException
     {
+        // 从请求头中获取客户端类型
         String clientTypeHeader = request.getHeader(CLIENT_TYPE);
+
+        // 检查是否为咨询用户，并进行身份验证
         if(StringUtils.isNotEmpty(clientType) && clientType.equals(clientTypeHeader)){
+            // 获取当前登录的咨询用户
             ConsultDTO consultUser = consultantTokenService.getLoginUser(request);
+            // 当咨询用户存在且当前认证为空时，进行身份验证
             if (StringUtils.isNotNull(consultUser) && StringUtils.isNull(SecurityUtils.getAuthentication()))
             {
+                // 验证用户令牌
                 consultantTokenService.verifyToken(consultUser);
+                // 创建并设置认证令牌
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(consultUser, null, consultUser.getAuthorities());
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
+        // 检查是否为普通用户，并进行身份验证
         }else{
+            // 获取当前登录的普通用户
             LoginUser loginUser = tokenService.getLoginUser(request);
+            // 当普通用户存在且当前认证为空时，进行身份验证
             if (StringUtils.isNotNull(loginUser) && StringUtils.isNull(SecurityUtils.getAuthentication()))
             {
+                // 验证用户令牌
                 tokenService.verifyToken(loginUser);
+                // 创建并设置认证令牌
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
+
         chain.doFilter(request, response);
     }
 }
