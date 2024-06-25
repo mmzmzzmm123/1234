f97579d7eb0b03dbb8f8d7ea4ca0f48a58b625b1
xhlvk <xhlvk@isoftstone.com>
2024-06-25 11:33:35 +0800

    字典更新

--- sql/update/update.sql
+++ sql/update/update.sql
@@ -2,3 +2,4 @@ ALTER TABLE psy_consult_work ADD COLUMN consult_type CHAR(4) NOT NULL DEFAULT '1
 update psy_consult_work set consult_type = '1' where consult_type is null;
 
 ALTER TABLE psy_consult_order ADD COLUMN consult_type CHAR(4) NOT NULL DEFAULT '1' COMMENT '1 咨询 2倾听 3督导' AFTER nick_name;
+ALTER TABLE psy_consult ADD COLUMN team_type CHAR(4) NOT NULL DEFAULT '' COMMENT '团督类型: 1 个督 2团督 '  AFTER user_id;
